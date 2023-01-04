d9a667cf0a0a8787cdc892c17dbd4d95b15c2fd2
finlaywashere <finman292004@protonmail.com>
2023-01-03 21:02:12 -0500

    Added migrations and fixed some bugs in models

--- euchre_models.py
+++ euchre_models.py
@@ -7,11 +7,22 @@ class EuchreCard(models.Model):
     rank = models.IntegerField('rank')
     player = models.ForeignKey('EuchrePlayer', related_name='card', on_delete=models.CASCADE, null=True)
     played = models.BooleanField('Played', default=False)
+    @property
+    def name(self):
+        suits = {'H': '♥️', 'D': '♦️', 'C': '♣️', 'S': '♠️'}
+        ranks = {14: 'Ace', 13: 'King', 12: 'Queen', 11: 'Jack'}
+        if self.rank > 10:
+            return suits[self.suit] + " " + ranks[self.rank]
+        else:
+            return suits[self.suit] + " " + str(self.rank)
+
 
 class EuchreTrick(models.Model):
-    opener = models.ForeignKey('EuchreCard', related_name='trick_opener', on_delete=models.CASCADE)
+    opener = models.ForeignKey('EuchreCard', related_name='trick_opener', on_delete=models.CASCADE, null=True)
     highest = models.ForeignKey('EuchreCard', related_name='trick', on_delete=models.CASCADE, null=True)
     selection = models.BooleanField('Trump Selection', default=False)
+    count = models.IntegerField('Count', default=0)  # used for counting misc stuff eg how many passes in selection
+
 
 class EuchrePlayer(models.Model):
     id = models.PositiveBigIntegerField("Player's Discord ID", primary_key=True)
@@ -27,6 +38,7 @@ class EuchrePlayer(models.Model):
         if len(cards) > 0:
             return True
         return False
+
 
 class EuchreTeam(models.Model):
     id = models.AutoField("Team ID", primary_key=True)
@@ -45,10 +57,12 @@ class EuchreGame(models.Model):
     end = models.DateTimeField('Time Finished', default=None, null=True)
     dealer = models.ForeignKey('EuchrePlayer', on_delete=models.CASCADE)
     next_dealer = models.ForeignKey('EuchrePlayer', related_name='next_dealer', on_delete=models.CASCADE)
+    selector = models.ForeignKey('EuchrePlayer', related_name='selector', on_delete=models.CASCADE, null=True)
     trump = models.CharField('Suit', max_length=1)
     current_trick = models.ForeignKey('EuchreTrick', on_delete=models.CASCADE)
     winner = models.ForeignKey('EuchreTeam', related_name='game_winner', on_delete=models.CASCADE, null=True)
     declarer = models.ForeignKey('EuchreTeam', related_name='game_declarer', on_delete=models.CASCADE, null=True)
+    extra_card = models.ForeignKey('EuchreCard', on_delete=models.CASCADE, null=True)
 
     @property
     def is_completed(self) -> bool:
@@ -60,6 +74,23 @@ class EuchreGame(models.Model):
         team_members = EuchrePlayer.objects.filter(team=dealer_team)
         new_dealer = team_members.exclude(id=current.id).first()
         return new_dealer
+    def compute_selector(self) -> EuchrePlayer:
+        selector = None
+        if self.selector == self.dealer:
+            selector = self.next_dealer
+        elif self.selector == self.next_dealer:
+            selector = self.compute_dealers(self.dealer)
+        elif self.selector == self.compute_dealers(self.dealer):
+            selector = self.compute_dealers(self.next_dealer)
+        else:
+            selector = self.dealer
+
+        team = selector.team
+        self.selector = selector
+        self.save()
+        if team.is_going_alone and team.going_alone != selector:
+            selector = compute_selector()
+        return selector
 
     @property
     def points(self):
